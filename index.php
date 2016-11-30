6fb58da4580dc946e8fa4e4dc07044e409655399
cesarZubillaga <cesar.zubillaga85@gmail.com>
2016-11-30 01:30:43 +0100

    wkhtmltopdf, that's what i like.

--- index.php
+++ index.php
@@ -1,19 +1,17 @@
 <?php
+use mikehaertl\wkhtmlto\Pdf;
+exec('/home/cesar/Downloads/wkhtmltox/bin/wkhtmltopdf files.local/content.html --header-html files.local/header.html --footer-html files.local/footer.html content.pdf');
+
 include 'vendor/autoload.php';
-include 'PdfClient.php';
-$html = file_get_contents('layout.html');
-$html = str_replace('{{imgdir}}', './img', $html);
-$file = sprintf('%s/file_%s.html', __DIR__, date('d-m-h'));
-$html = file_get_contents('http://www.sbb-immobilien.ch/centre-loewenberg');
-file_put_contents($file, $html);
-$pdf = new \mikehaertl\wkhtmlto\Pdf('http://www.sbb-immobilien.ch/centre-loewenberg');
-$pdf->binary = '/home/cesar/wkhtmltox/bin/wkhtmltopdf';
+
+$pdf = new Pdf();
+$pdf->addPage('files.local/content.html');
+$pdf->setOptions(array(
+    'header-html' => 'files.local/header.html',
+    'footer-html' => 'files.local/footer.html'
+));
+
+$pdf->binary = '/home/cesar/Downloads/wkhtmltox/bin/wkhtmltopdf';
 if (false === $pdf->saveAs('./my_pdf.pdf')) {
     echo $pdf->getError();
 }
-$html = file_get_contents($file);
-$pdf = new \Dompdf\Dompdf();
-$pdf->loadHtml($html);
-$pdf->setPaper('A4');
-$pdf->render();
-$pdf->stream('filename');
